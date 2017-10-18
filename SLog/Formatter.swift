21897a0abd6593517f7e9c551c3d919eb20aa142
Sam Krishna <sam.krishna@gmail.com>
2017-10-18 10:52:49 -0700

    Initial architecture commit.

---
+++ SLog/Formatter.swift
@@ -0,0 +1,9 @@
+//
+//  Formatter.swift
+//  SLog
+//
+//  Created by Sam Krishna on 10/17/17.
+//  Copyright © 2017 Sam Krishna. All rights reserved.
+//
+
+import Foundation
---
+++ SLog/LogRecord.swift
@@ -0,0 +1,11 @@
+//
+//  LogMessage.swift
+//  SLog
+//
+//  Created by Sam Krishna on 10/17/17.
+//  Copyright © 2017 Sam Krishna. All rights reserved.
+//
+
+import Foundation
+
+
