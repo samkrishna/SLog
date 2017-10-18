365aa5b0c1f4f3e3f475133879f76e0e692f361e
Sam Krishna <sam.krishna@gmail.com>
2017-10-18 10:53:55 -0700

    Started LogSeverity

--- SLog/LogSeverity.swift
+++ SLog/LogSeverity.swift
@@ -7,3 +7,30 @@
 //
 
 import Foundation
+
+public func <(lhs: LogSeverity, rhs: LogSeverity) -> Bool {
+    return lhs.rawValue < rhs.rawValue
+}
+
+public func ==(lhs: LogSeverity, rhs: LogSeverity) -> Bool {
+    return lhs.rawValue == rhs.rawValue
+}
+
+public enum LogSeverity: UInt {
+    case verbose    = 0
+    case debug      = 1
+    case info       = 2
+    case warning    = 3
+    case error      = 4
+
+    public var description: String {
+        switch self {
+        case .verbose:      return "Verbose"
+        case .debug:        return "Debug"
+        case .info:         return "Info"
+        case .warning:      return "Warning"
+        case .error:        return "Error"
+        }
+    }
+}
+
